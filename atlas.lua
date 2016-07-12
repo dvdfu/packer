87e29f3394a12675dda5aacd2627353f92c6c0d6
David Fu <d23fu@uwaterloo.ca>
2016-07-12 00:02:44 -0700

    quad split for animations

--- atlas.lua
+++ atlas.lua
@@ -13,6 +13,14 @@ function Atlas.load(filename)
 
     local atlas = {
         quads = quads,
+        quadSplit = function(self, sprite, n)
+            local x, y, w, h = self.quads[sprite]:getViewport()
+            local row = {}
+            for i = 1, n do
+                row[i] = love.graphics.newQuad(x + (i - 1) * w / n, y, w / n, h, sw, sh)
+            end
+            return row
+        end,
         draw = function(self, sprite, x, y, r, sx, sy, ox, oy, kx, ky)
             love.graphics.draw(image, self.quads[sprite], x, y, r, sx, sy, ox, oy, kx, ky)
         end
