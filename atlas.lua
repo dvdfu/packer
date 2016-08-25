ec6c32f2c36b60d8f55fef7217e45d9a69afb633
David Fu <dfu@yahoo-inc.com>
2016-08-25 09:09:51 -0700

    animation class

--- atlas.lua
+++ atlas.lua
@@ -1,15 +1,39 @@
-local Sprite = {}
-
-function Sprite.new(source, quad)
+function Sprite(source, quad)
     return {
         source = source,
         quad = quad,
+        getQuad = function(self)
+            return self.quad
+        end,
         draw = function(self, x, y, r, sx, sy, ox, oy, kx, ky)
-            if self.quad then
-                love.graphics.draw(self.source, self.quad, math.floor(x), math.floor(y), r, sx, sy, ox, oy, kx, ky)
-            else
-                love.graphics.draw(self.source, math.floor(x), math.floor(y), r, sx, sy, ox, oy, kx, ky)
+            love.graphics.draw(self.source, self.quad, math.floor(x), math.floor(y), r, sx, sy, ox, oy, kx, ky)
+        end
+    }
+end
+
+function Animation(source, quads, framerate)
+    return {
+        source = source,
+        quads = quads,
+        framerate = framerate,
+        timer = 0,
+        frame = 1,
+        getQuads = function(self)
+            return self.quads
+        end,
+        update = function(self, dt)
+            self.timer = self.timer + dt
+            while self.timer > self.framerate do
+                self.timer = self.timer - self.framerate
+                self.frame = self.frame + 1
+                if self.frame > #self.quads then
+                    self.frame = self.frame - #self.quads
+                end
             end
+        end,
+        draw = function(self, x, y, r, sx, sy, ox, oy, kx, ky)
+            local quad = self.quads[self.frame]
+            love.graphics.draw(self.source, quad, math.floor(x), math.floor(y), r, sx, sy, ox, oy, kx, ky)
         end
     }
 end
@@ -36,20 +60,21 @@ function Atlas.load(filename)
         data = data.images,
         image = image,
         quads = quads,
-        quadSplit = function(self, sprite, n)
-            local x, y, w, h = self.quads[sprite]:getViewport()
-            local row = {}
-            for i = 1, n do
-                row[i] = love.graphics.newQuad(x + (i - 1) * w / n, y, w / n, h, sw, sh)
-            end
-            return row
-        end,
         draw = function(self, sprite, x, y, r, sx, sy, ox, oy, kx, ky)
             love.graphics.draw(image, self.quads[sprite], x, y, r, sx, sy, ox, oy, kx, ky)
         end,
         newSprite = function(self, name)
             assert(self.quads[name])
-            return Sprite.new(self.image, self.quads[name])
+            return Sprite(self.image, self.quads[name])
+        end,
+        newAnimation = function(self, name, n, framerate)
+            assert(self.quads[name])
+            local x, y, w, h = self.quads[name]:getViewport()
+            local quads = {}
+            for i = 1, n do
+                quads[i] = love.graphics.newQuad(x + (i - 1) * w / n, y, w / n, h, sw, sh)
+            end
+            return Animation(self.image, quads, framerate)
         end
     }
     return atlas
