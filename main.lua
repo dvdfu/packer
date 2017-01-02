0d1faf8e42772ab0b2dd13a08679169a521cf074
David Fu <davidf1212@gmail.com>
2017-01-02 16:00:03 -0500

    no atlas display

--- main.lua
+++ main.lua
@@ -1,20 +1,9 @@
 local Pack = require 'pack'
 
-local image
-
 function love.load(arg)
     local source = arg[2]
     local output = arg[3] or 'out'
 
-    local atlas = Pack(source, output)
-    love.window.setMode(atlas.size, atlas.size)
-    image = love.graphics.newImage(atlas.image)
-end
-
-function love.draw()
-    love.graphics.draw(image)
-end
-
-function love.keypressed()
+    Pack(source, output)
     love.event.push('quit')
 end
